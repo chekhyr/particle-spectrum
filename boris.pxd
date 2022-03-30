351237e50a42458f92976d808336f1ff11031669
morniehtar <46871606+morniehtar@users.noreply.github.com>
2022-03-30 18:26:10 +0300

    imported some of boris.so stuff into intencity.so

---
+++ boris.pxd
@@ -0,0 +1,3 @@
+# cython: language_level=3str
+cdef double dot(double[:] vec1, double[:] vec2)
+cdef double cross(double[:] vec1, double[:] vec2, int j)
--- setup.py
+++ setup.py
@@ -6,10 +6,11 @@ import numpy
 extensions = [
     Extension(
         'intencity',
-        ['intencity.pyx'],
+        ['intencity.pyx', 'boris.pyx'],
         #extra_compile_args=['-fopenmp'],
         include_dirs=[numpy.get_include()],
-        define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')]
+        define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')],
+        
     )
 ]
 

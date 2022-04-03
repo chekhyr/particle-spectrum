dbf173e1d711d3c740f25edabd323418c26bf0c0
morniehtar <46871606+morniehtar@users.noreply.github.com>
2022-04-03 13:44:43 +0300

    fin!

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from spectrum import spectrum_routine
 from plotter import PlotTrajectory, PlotSpectrum
 import numpy as np
 
+import matplotlib.pyplot as plt
+
 # Config
 nt = 1000
 t_span = (0., 200.)
@@ -12,9 +14,7 @@ q = 1.
 m = 1.
 x0 = np.array([0, 0, 0]).astype(np.double)
 p0 = np.array([0.9, 0, 0]).astype(np.double)
-
 Radiation = True
-
 
 objPtcl = Particle(q, m, x0, p0)
 objEMF = EMF(0)
@@ -22,7 +22,8 @@ objEMF = EMF(0)
 trj = boris_routine(objPtcl, objEMF, t_span, nt, Radiation)
 
 objPlt = PlotTrajectory(trj[0], trj[1])
-objPlt.space()
+#objPlt.space()
+
 
 n = np.array([0, 1, 0]).astype(np.double)
 omg_span = (0., 4.)
@@ -30,4 +31,28 @@ nOmg = 500
 spct = spectrum_routine(trj[0], trj[1], trj[2], trj[3], n, omg_span, nOmg, objPtcl)
 
 objSpct = PlotSpectrum(spct[0], spct[1])
-objSpct.draw()
+#objSpct.draw()
+
+
+tht_span = (0., np.pi)
+nTht = 100
+
+tht = np.linspace(tht_span[0], tht_span[1], nTht)
+heatmp = np.zeros((nTht, nOmg))
+for i in range(nTht):
+    n[0] = np.sin(tht[i])
+    n[1] = 0
+    n[2] = np.cos(tht[i])
+    heatmp[i][:] = spectrum_routine(trj[0], trj[1], trj[2], trj[3], n, omg_span, nOmg, objPtcl)[1][:]
+else:
+    omg = np.linspace(omg_span[0], omg_span[1], nOmg)
+
+X, Y = np.meshgrid(omg, tht)
+
+plt.subplot(projection="polar")
+plt.grid(False)
+plt.pcolormesh(Y, X, heatmp)
+#plt.grid()
+plt.colorbar()
+plt.tight_layout()
+plt.show()
